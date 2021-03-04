3a645d6cecaa5c3a4ae9907ee670c0d0da7accdf
RyoRandy <78986950+RyoRandy@users.noreply.github.com>
2021-03-04 07:50:03 -0600

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITLA_Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worlda!");
+        }
+    }
+}
