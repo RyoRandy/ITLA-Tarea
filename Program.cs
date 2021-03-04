9f0ccca9b303ab4cc98512f6036f9ddbf3b2c9e1
RyoRandy <78986950+RyoRandy@users.noreply.github.com>
2021-03-04 09:52:38 -0600

    Tercer Array

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace ITLA_Tarea
         static void Main(string[] args)
         {
             PrimerArray();
-            //SegundoArray();
-            //TercerArray();
+            SegundoArray();
+            TercerArray();
             //CuartoArray();
             //QuintoArray();
             //SextoArray();
@@ -18,10 +18,12 @@ namespace ITLA_Tarea
             //NovenoArray();
             //DecimoArray();
         }
+        #region PrimerArray
         static void PrimerArray(){
             double media = 0;
             int cantidad = 4;
             int[] numeros = new int[cantidad];
+            Console.WriteLine("Programa #1 - Bienvenido.");
             Console.WriteLine("Ingrese los cuatro números.");
             for (int i = 0; i < numeros.Length; i++){
                 numeros[i] = Convert.ToInt32(Console.ReadLine());
@@ -30,9 +32,68 @@ namespace ITLA_Tarea
             media /= cantidad;
             Console.WriteLine($"Los números en el array son:");
             for (int i = 0; i < numeros.Length; i++){
-                Console.Write(numeros[i] + " ");
+                Console.Write(numeros[i] + ", ");
             }
             Console.WriteLine($"La media aritmetica es: {media}");
+            Console.WriteLine("Fin del Programa #1 \n" + 
+            "-------");
         }
+        #endregion
+
+        #region SegundoArray
+        static void SegundoArray(){
+            int cantidad = 5;
+            float[] reales = new float[cantidad];
+            
+            Console.WriteLine("Programa #2 - Bienvenido.");
+            Console.WriteLine("Ingrese los cinco números");
+            for(int i = 0; i < reales.Length; i++){
+                reales[i] = float.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("El orden normal de los números es el siguiente: ");
+            for(int i = 0; i < reales.Length; i++) {
+                Console.Write(reales[i] + " ");
+            }
+            Console.WriteLine("El orden contrario es: ");
+            for(int i = reales.Length - 1; i >= 0; i--){
+                Console.Write(reales[i] + " ");
+            }
+            Console.WriteLine("Fin del Programa #2 \n" + 
+            "-------");
+        }
+        #endregion
+
+        #region TercerArray
+        static void TercerArray(){
+            Console.WriteLine("Programa #3 - Bienvenido.");
+            int[] dias = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            string[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+            Console.WriteLine("Ingrese en base a los números de la lista al mes que desea saber sus días. \n" + 
+            "Seleccione un mes. \n" +
+            "1-) Enero \n" + 
+            "2-) Febrero \n" + 
+            "3-) Marzo \n" +
+            "4-) Abril \n" + 
+            "5-) Mayo \n" + 
+            "6-) Junio \n" +
+            "7-) Julio \n" + 
+            "8-) Agosto \n" + 
+            "9-) Septiembre \n" +
+            "10-) Octubre \n" + 
+            "11-) Noviembre \n" + 
+            "12-) Diciembre");
+            int mes = Convert.ToInt32(Console.ReadLine());
+            if (mes > meses.Length || mes <= 0){
+                Console.WriteLine("El mes ingresado no existe, intentelo nuevamente");
+                mes = Convert.ToInt32(Console.ReadLine());
+            }
+            else {
+                mes -= 1;
+                Console.WriteLine($"El mes que elegiste es {meses[mes]} y tiene {dias[mes]} días");
+            }
+            Console.WriteLine("Fin del Programa #3 \n" + 
+            "-------");
+        }
+        #endregion
     }
 }
